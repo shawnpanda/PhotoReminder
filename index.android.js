c17dde180882983a35afbf04ff245cdf3774db83
Shawn Pan <xpan1@swarthmore.edu>
2016-10-01 20:53:46 -0400

    photoreminder app init

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * Photo Reminder App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import PhotoReminder from './app/App'
+
+AppRegistry.registerComponent('PhotoReminder', () => PhotoReminder);
